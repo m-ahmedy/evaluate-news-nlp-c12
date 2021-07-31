7f5661a8c9bdf9b20566ba42744419053ee78768
Mahmoud Ahmedy <m7m9d.a7medy@gmail.com>
2021-07-31 18:14:36 +0200

    Add: Todos

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,19 +1,12 @@
-import { checkForName } from './nameChecker'
+import { isValidUrl } from './urlChecker'
 
 function handleSubmit(event) {
-    event.preventDefault()
-    console.log({ event })
-
-    // check what text was put into the form field
-    let formText = document.getElementById('name').value
-    checkForName(formText)
-
-    console.log("::: Form Submitted :::")
-    fetch('http://localhost:8081/test')
-        .then(rawResponse => rawResponse.json())
-        .then(function (response) {
-            document.getElementById('results').innerHTML = response.message
-        })
+    // TODO: Stop Default behaviour (Page Refresh)
+    // TODO: Get article url from the form
+    // TODO: Check if the url is indeed a valid url
+    // TODO: If the url is invalid, Show an alert
+    // TODO: If the url is valid, make a request to the server
+    // TODO: Display the response on the UI Appropriately    
 }
 
 export { handleSubmit }
---
+++ src/client/js/urlChecker.js
@@ -0,0 +1,5 @@
+function isValidUrl(inputText) {
+    // TODO: return true if the input text is a valid URL, or false if not
+}
+
+export { isValidUrl }
